0c7fd71d2d67f5ecac2d86dd0176bf66d7b547e4
Boti <Boti@Boti-PC>
2017-09-08 15:23:59 +0200

    minor changes, DFP test, drools version upgrade

---
+++ src/test/java/onlab/utility/DataFileParserTest.java
@@ -0,0 +1,115 @@
+package onlab.utility;
+
+import static org.junit.Assert.*;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class DataFileParserTest {
+
+	public static File TEST_CSV = new File("testCSV.csv");
+	public static SimpleDateFormat DATEFORMAT = DataFileParser.SIMPLE_DATE_FORMAT;
+	public static String MEDALLION = "medallion";
+	public static String HACK_LICENSE = "hack_license";
+	private static Date PICKUP_DATETIME = new Date();
+	private static Date DROPOFF_DATETIME = (Date) PICKUP_DATETIME.clone();
+	private static String TRIP_TIME_IN_SECS = "0";
+	private static String TRIP_DISTANCE = "0";
+	private static String PICKUP_X_COORDINATE = "0";
+	private static String PICKUP_Y_COORDINATE = "1";
+	private static String DROPOFF_X_COORDINATE = "2";
+	private static String DROPOFF_Y_COORDINATE = "3";
+	private static String PAYMENT_TYPE = "CSH";
+	private static String FARE_AMOUNT = "1";
+	private static String SURCHARGE = "2";
+	private static String MTA_TAX = "3";
+	private static String TIP_AMOUNT = "4";
+	private static String TOLLS_AMOUNT = "5";
+	private static String TOTAL_AMOUNT = "6";
+	private static String SEPARATOR = ";";
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+
+		try(BufferedWriter writer = new BufferedWriter(new FileWriter(TEST_CSV))){
+			Calendar dropoff = Calendar.getInstance();
+			dropoff.setTime(DROPOFF_DATETIME);
+			for(int i = 0; i < 4; i++) {
+				
+				
+				for(int j = 0; j <= i; j++) {
+					
+					StringBuilder sb = new StringBuilder(MEDALLION+SEPARATOR);
+					sb.append(HACK_LICENSE+SEPARATOR);
+					sb.append(DATEFORMAT.format(PICKUP_DATETIME)+SEPARATOR);
+					sb.append(DATEFORMAT.format(dropoff.getTime())+SEPARATOR);
+					sb.append(TRIP_TIME_IN_SECS+SEPARATOR);
+					sb.append(TRIP_DISTANCE+SEPARATOR);
+					sb.append(PICKUP_X_COORDINATE+SEPARATOR);
+					sb.append(PICKUP_Y_COORDINATE+SEPARATOR);
+					sb.append(DROPOFF_X_COORDINATE+SEPARATOR);
+					sb.append(DROPOFF_Y_COORDINATE+SEPARATOR);
+					sb.append(PAYMENT_TYPE+SEPARATOR);
+					sb.append(FARE_AMOUNT+SEPARATOR);
+					sb.append(SURCHARGE+SEPARATOR);
+					sb.append(MTA_TAX+SEPARATOR);
+					sb.append(TIP_AMOUNT+SEPARATOR);
+					sb.append(TOLLS_AMOUNT+SEPARATOR);
+					sb.append(TOTAL_AMOUNT);
+					
+					writer.write(sb.toString());
+					writer.newLine();
+					
+				}
+				
+				dropoff.add(Calendar.MINUTE, 1);
+				
+				
+			}	
+		}
+		
+		
+		
+
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		
+		TEST_CSV.delete();
+	} 
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testParseNextLinesFromCSVGroupedByDropoffDate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testParseCSVIntoTaxiLogListStringStringIntIntCellHelperInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testParseCSVIntoTaxiLogListStringStringIntCellHelper() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testParseCSVIntoTaxiLogListStringStringIntIntCellHelper() {
+		fail("Not yet implemented");
+	}
+
+}
